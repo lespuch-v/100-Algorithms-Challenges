8cf2564327eb4104130f604fc5ca10987d160776
lespuch-v <36127590+lespuch-v@users.noreply.github.com>
2021-12-21 19:06:58 +0100

    Update 1. absoluteValuesSumMinization -solution.js

--- 1. absoluteValuesSumMinization/1. absoluteValuesSumMinization -solution.js
+++ 1. absoluteValuesSumMinization/1. absoluteValuesSumMinization -solution.js
@@ -1,1 +1,7 @@
-console.log(1+2)
+function myFunk(array){
+    const isEvent = array.length % 2 === 0
+    return isEvent ? array[array.length / 2 - 1] : array[Math.floor(array.length / 2)]
+}
+
+console.log(myFunk([2,4,7,6]))
+console.log(myFunk([2,4,7,6,7,6,6]))
