bb240968dec183c9c8a2bf1f5bcb097044f1e19e
lespuch-v <36127590+lespuch-v@users.noreply.github.com>
2022-02-18 05:33:29 +0100

    upload
    ts

---
+++ 43. depositProfit/script.ts
@@ -0,0 +1,10 @@
+export function depositProfit(deposit: number, rate: number, threshold: number): number {
+
+}
+
+// console.log(depositProfit(100, 20, 170))
+
+// You will need
+// while loop
+// two variables
+// simple logic
